2d6c65f8e549ac5361f70cc55d02f48e7ab84176
mehtaAlok <alokmehta901@gmail.com>
2022-12-27 23:27:58 +0000

    courseinfo 1.2 complete

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -9,19 +9,42 @@ const App = () => {
 
   return (
     <div>
-      <h1>{course}</h1>
-      <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
-      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+      <Header course={course} />
+      <Content
+        part1={part1}
+        exercise1={exercises1}
+        part2={part2}
+        exercise2={exercises2}
+        part3={part3}
+        exercise3={exercises3}
+      />
+      <Total exercises={exercises1 + exercises2 + exercises3} />
     </div>
   )
 }
+
+const Header = (prop) => <h1>{prop.course}</h1>
+
+const Content = ({ part1, part2, part3, exercise1, exercise2, exercise3 }) => {
+  // console.log(props)
+
+  return (
+    <div>
+      <Part part={part1} exercise={exercise1} />
+      <Part part={part2} exercise={exercise2} />
+      <Part part={part3} exercise={exercise3} />
+    </div>
+  )
+}
+
+const Part = (props) => {
+  return (
+    <p>
+      {props.part} {props.exercise}
+    </p>
+  )
+}
+
+const Total = (props) => <p>Number of exercises {props.exercises}</p>
 
 export default App
