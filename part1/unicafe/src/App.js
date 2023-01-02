7971029e7be3f6a70aa1c881074003755a510630
mehtaAlok <alokmehta901@gmail.com>
2023-01-02 02:38:36 +0000

    Completed unicafe 1.7

---
+++ part1/unicafe/src/App.js
@@ -0,0 +1,43 @@
+import { useState } from "react"
+
+const Display = (props) => {
+  return (
+    <>
+      <br />
+      <div>
+        {props.text} {props.count}
+      </div>
+    </>
+  )
+}
+
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  const increaseGood = () => setGood(good + 1)
+  const increaseNeutral = () => setNeutral(neutral + 1)
+  const increaseBad = () => setBad(bad + 1)
+
+  return (
+    <div>
+      <font size="+2">Give Feedback</font>
+      <br />
+      <br />
+      <button onClick={increaseGood}>Good</button>
+      <button onClick={increaseNeutral}>Neutral</button>
+      <button onClick={increaseBad}>Bad</button>
+      <br />
+      <br />
+      <font size="+2">Statistics</font>
+      <br />
+      <Display count={good} text="Good" />
+      <Display count={neutral} text="Neutral" />
+      <Display count={bad} text="Bad" />
+    </div>
+  )
+}
+
+export default App
