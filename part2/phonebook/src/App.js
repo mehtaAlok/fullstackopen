c281c410b019e24d2e79d54b35df5c3048df9de7
mehtaAlok <alokmehta901@gmail.com>
2023-01-09 21:46:14 +0000

    Completed courseinfo 2.8

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,30 +1,46 @@
 import { useState } from "react"
 
 const Person = (person) => {
-  return <li>{person.person.name}</li>
+  return (
+    <li>
+      {person.person.name} {person.person.phone}
+    </li>
+  )
 }
 
 const App = () => {
-  const [persons, setPersons] = useState([{ name: "Arto Hellas" }])
+  const [persons, setPersons] = useState([
+    { name: "Arto Hellas", phone: "040-1234567" }
+  ])
   const [newName, setNewName] = useState("")
+  const [newNumber, setNumber] = useState("")
 
   const addPerson = (e) => {
     e.preventDefault()
     const personObject = { name: newName }
-    const found = persons.find(
-      (e) => JSON.stringify(e) === JSON.stringify(personObject)
-    )
-    if (JSON.stringify(personObject) === JSON.stringify(found)) {
+    const phoneObject = { phone: newNumber }
+    console.log("new name is", newName)
+    const found = persons.find((e) => {
+      console.log("what is e", e.name)
+      return e.name === newName
+    })
+    if (found) {
       alert(`${newName} is already added to the phonebook`)
       setNewName("")
       return
     }
-    setPersons(persons.concat(personObject))
+    const newPersonObj = [{ ...personObject, ...phoneObject }]
+    setPersons(persons.concat(newPersonObj))
     setNewName("")
+    setNumber("")
   }
 
   const handleChange = (e) => {
     setNewName(e.target.value)
+  }
+
+  const handlePhoneChange = (e) => {
+    setNumber(e.target.value)
   }
 
   return (
@@ -32,12 +48,17 @@ const App = () => {
       <h2>Phonebook</h2>
       <form onSubmit={addPerson}>
         <div>
-          name: <input value={newName} onChange={handleChange} />
+          Name: <input value={newName} onChange={handleChange} />
         </div>
+        <div>
+          Number: <input value={newNumber} onChange={handlePhoneChange} />
+        </div>
+
         <div>
           <button type="submit">add</button>
         </div>
       </form>
+
       <h2>Numbers</h2>
 
       <ul>
