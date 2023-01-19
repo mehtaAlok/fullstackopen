f3143ba798032888ca43410d7df4870c76f42e65
mehtaAlok <alokmehta901@gmail.com>
2023-01-19 03:02:54 +0000

    Completed phonebook 3.5

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -1,6 +1,11 @@
 const { response } = require("express")
 const express = require("express")
 const app = express()
+app.use(express.json())
+
+const generateId = () => {
+  return Math.floor(Math.random() * 10000)
+}
 
 let persons = [
   {
@@ -55,6 +60,24 @@ app.delete("/api/persons/:id", (request, response) => {
   const id = Number(request.params.id)
   persons = persons.filter((person) => person.id !== id)
   response.status(204).end()
+})
+
+app.post("/api/persons", (request, response) => {
+  const body = request.body
+  console.log("Body is", body)
+
+  if (!body) {
+    return response.status(400).json({ error: "Content missing" })
+  }
+
+  const person = {
+    id: generateId(),
+    name: body.name,
+    number: body.number
+  }
+
+  persons = persons.concat(person)
+  response.json(persons)
 })
 
 const PORT = 3001
